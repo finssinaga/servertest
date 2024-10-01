3053bdada57f2886d67e45c7310dfccdb03c07e6
finssinaga <finssinaga@gmaill.com>
2024-10-01 16:52:26 +0700

    -

--- server/src/server/Pipe.java
+++ server/src/server/Pipe.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Base64;
 
 public class Pipe implements Runnable{
@@ -42,7 +46,7 @@ public Pipe(Socket sock){
 					broadcast(ms.substring(ms.indexOf(">")+1), so);
 					MainMenu.getjtext().setText(MainMenu.getjtext().getText()+"\n"+ms);
 				}
-				if(ms.contains("#")) {
+				if(ms.contains("[REG]")) {
 					out.println("Welcome "+ms.substring(0,ms.indexOf("#"))+". Current online user : "+MainMenu.client.size());
 					MainMenu.getjtext().setText(MainMenu.getjtext().getText()+"\n"+"User Login : "+ms.substring(0,ms.indexOf("#")));
 				}
@@ -53,5 +57,24 @@ public Pipe(Socket sock){
 			MainMenu.getjtext().setText(MainMenu.getjtext().getText()+"\n"+e);
 		}
 	}
-	
+public String setuser(String user, String pass) {
+	String usr,pas;
+	usr=null;
+	pass = null;
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/chat","chat","chatter");
+		Statement stat = con.createStatement();
+		ResultSet res = stat.executeQuery("select * from user where username='"+user+"' and password='"+"'");
+		while(res.next()) {
+			usr=res.getString(1);
+			pass=res.getString(2);
+		}
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	String com = usr+"0"+pass;
+	return com;
+}
 }
